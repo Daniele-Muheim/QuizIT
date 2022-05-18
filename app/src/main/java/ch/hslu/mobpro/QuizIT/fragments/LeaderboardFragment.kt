030bd9ffd7b254100ef468567287851e5695fc56
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-05-18 18:12:19 +0200

    implemented requesting from api

--- app/src/main/java/ch/hslu/mobpro/QuizIT/fragments/LeaderboardFragment.kt
+++ app/src/main/java/ch/hslu/mobpro/QuizIT/fragments/LeaderboardFragment.kt
@@ -1,13 +1,18 @@
 package ch.hslu.mobpro.QuizIT.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import ch.hslu.mobpro.QuizIT.Leaderboard
+import ch.hslu.mobpro.QuizIT.QuizViewModel
 import ch.hslu.mobpro.QuizIT.R
+import ch.hslu.mobpro.QuizIT.Score
 import ch.hslu.mobpro.QuizIT.adapters.ItemAdapter
 import ch.hslu.mobpro.QuizIT.databinding.FragmentLeaderboardBinding
 
@@ -15,9 +20,13 @@ import ch.hslu.mobpro.QuizIT.databinding.FragmentLeaderboardBinding
 class LeaderboardFragment : Fragment(R.layout.fragment_leaderboard) {
     private lateinit var viewBinding: FragmentLeaderboardBinding
     private  val binding get() = viewBinding
+    private val quizViewModel: QuizViewModel by activityViewModels()
+    private var scoresAsList: List<Score>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestLeaderboard()
     }
 
     companion object {
@@ -27,14 +36,35 @@ class LeaderboardFragment : Fragment(R.layout.fragment_leaderboard) {
         }
     }
 
-    private fun getItemsList(): ArrayList<String> {
+    private fun getUsernameList(): ArrayList<String> {
         val list = ArrayList<String>()
 
-        for (i in 1..10) {
-            list.add("Item $i")
+        for (Score in scoresAsList!!) {
+            list.add(Score.username)
         }
         return list
     }
+    private fun getScoreList(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        for (Score in scoresAsList!!) {
+            list.add(Score.score.toString())
+        }
+        return list
+    }
+
+    private fun getTimeList(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        for (Score in scoresAsList!!) {
+            var minutes: Long = ((Score.timeInMilliseconds/1000)/60)
+            var second: Long = ((Score.timeInMilliseconds/1000)%60)
+            var payload: String = minutes.toString() + "min " + second.toString() +"sec"
+            list.add(payload)
+        }
+        return list
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,15 +75,20 @@ class LeaderboardFragment : Fragment(R.layout.fragment_leaderboard) {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        quizViewModel.leaderboard.observe(this) { apileaderboard ->
+            scoresAsList = apileaderboard
+            buildLeaderboard()
+
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val itemAdapter = ItemAdapter(getItemsList())
-        viewBinding.recyclerViewItems.adapter = itemAdapter
-        viewBinding.recyclerViewItems.layoutManager = LinearLayoutManager(this.context)
 
         viewBinding.startNewQuizButton.setOnClickListener { clickNewQuizButton() }
-
     }
 
     private fun clickNewQuizButton() {
@@ -64,4 +99,13 @@ class LeaderboardFragment : Fragment(R.layout.fragment_leaderboard) {
             .commit()
     }
 
+    private fun buildLeaderboard(){
+        val itemAdapter = ItemAdapter(getUsernameList(),getScoreList(), getTimeList())
+        viewBinding.recyclerViewItems.adapter = itemAdapter
+        viewBinding.recyclerViewItems.layoutManager = LinearLayoutManager(this.context)
+    }
+
+    private fun requestLeaderboard(){
+        quizViewModel.getLeaderBoard()
+    }
 }
